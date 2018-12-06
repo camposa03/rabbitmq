cabf17990be5c7035cdebecbe2ed2aa09e7482eb
camposa03 <camposa03@yahoo.com>
2018-12-05 22:26:58 -0700

    Add files via upload
    Initial commit

---
+++ Epic.Messaging.Consumer/Epic.Messaging.Consumer/CustomWebHostService.cs
@@ -0,0 +1,36 @@
+﻿using Epic.Rabbit.Subscriber;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.WindowsServices;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Epic.Messaging.Consumer
+{
+    internal class CustomWebHostService : WebHostService
+    {
+        private readonly ILogger _logger;
+        public CustomWebHostService(IWebHost host) : base(host)
+        {
+            _logger = host.Services.GetRequiredService<ILogger<CustomWebHostService>>();
+        }
+        protected override void OnStarting(string[] args)
+        {
+            _logger.LogInformation("OnStarting method called.");
+            base.OnStarting(args);
+        }
+        protected override void OnStarted()
+        {
+            _logger.LogInformation("OnStarted method called.");
+            base.OnStarted();
+
+            var subscriber = new Subscriber();
+            subscriber.ReceiveMessageAsync("test");
+        }
+
+        protected override void OnStopping()
+        {
+            _logger.LogInformation("OnStopping method called.");
+            base.OnStopping();
+        }
+    }
+}
