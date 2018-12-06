ab912e19d769269c683807d67bd2d4d00b3bcf3a
Armando Campos <camposa03@yahoo.com>
2018-12-05 22:12:34 -0700

    Initial commit

---
+++ Epic.Messaging.Consumer/Program.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epic.Messaging.Consumer
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+            var builder = CreateWebHostBuilder(
+                args.Where(arg => arg != "--console").ToArray());
+
+            if (isService)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                var pathToContentRoot = Path.GetDirectoryName(pathToExe);
+                builder.UseContentRoot(pathToContentRoot);
+            }
+
+            var host = builder.Build();
+
+            if (isService)
+            {
+                // To run the app without the CustomWebHostService change the
+                // next line to host.RunAsService();
+                host.RunAsCustomService();
+            }
+            else
+            {
+                await ExecuteAsync();
+
+                Console.ReadLine();
+
+            }
+
+
+        }
+
+        private static Task ExecuteAsync()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", DispatchConsumersAsync = true };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            {
+                channel.QueueDeclare(queue: "test",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                File.AppendAllText("C:\\Temp\\rabbit_subscriber.txt", "Created channel");
+
+                var consumer = new AsyncEventingBasicConsumer(channel);
+                consumer.Received += async (model, ea) =>
+                {
+                    await Task.Delay(5000);
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(message);
+
+                    File.AppendAllText("C:\\Temp\\rabbit_subscriber.txt", message);
+                    channel.BasicAck(ea.DeliveryTag, false);
+
+                    Console.WriteLine($"Acknowledged message with DeliveryTag: {ea.DeliveryTag}");             
+
+                };
+
+                channel.BasicConsume(queue: "test",
+                                     autoAck: false,
+                                     consumer: consumer);
+                
+            }
+
+            return Task.FromResult(0);
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
